67b032e5a28d4a378780c8bef2df7ee0f7ad43d1
Joseph Backelant's <backelantsjoseph@gmail.com>
2024-02-12 14:51:06 +0100

    correction de la baseURL

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:weather_app/model/ApiResponse.dart';
 import 'package:weather_app/model/GeoPosition.dart';
 
 import 'package:http/http.dart';
@@ -9,22 +10,22 @@ class ApiService {
   String baseUrl = "http://api.openweathermap.org/data/2.5/forecast";
   String lat = "lat=";
   String long = "lon=";
-  String appid = "aappid=";
+  String appid = "appid=";
   String lang = "lang=fr";
   String units = "units=metric";
 
   String prepareQuery(GeoPosition geoPosition) {
     final geoLat = geoPosition.lat;
     final geoLong = geoPosition.long;
-    return "$baseUrl&$lat$geoLat&$long$geoLong&$units$lang&$appid$apiKey";
+    return "$baseUrl?$lat$geoLat&$long$geoLong&$units$lang&$appid$apiKey";
   }
 
-  Future<Null> callApi(GeoPosition position) async {
+  Future<APIResponse> callApi(GeoPosition position) async {
     final queryString = prepareQuery(position);
     final uri = Uri.parse(queryString);
     final call = await get(uri);
 
     Map<String, dynamic> map = json.decode(call.body);
-    print(map);
+    return APIResponse.fromJson(map);
   }
 }
