8830d208b4b1d84ad9cd33107b4c8da121f5a498
Joseph Backelant's <backelantsjoseph@gmail.com>
2024-02-18 15:16:23 +0100

    ajout de la fonction fromIcon pour récupérer l'icon

--- lib/services/data_converter.dart
+++ lib/services/data_converter.dart
@@ -2,4 +2,8 @@ class DataConverter {
   List<Map<String, dynamic>> listMappable(List<dynamic> list) {
     return list.map((e) => e as Map<String, dynamic>).toList();
   }
+
+  String fromIcon(String string) {
+    return "http://openweathermap.org/img/wn/$string@2x.png";
+  }
 }
