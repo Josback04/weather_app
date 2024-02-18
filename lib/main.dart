8c14d56f238fa3239963c52d376dd689d42c5109
Joseph Backelant's <backelantsjoseph@gmail.com>
2024-02-18 15:15:26 +0100

    définir les themes pour chaque taille de texe

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,16 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(Typography.blackCupertino),
+        textTheme: GoogleFonts.poppinsTextTheme(TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).primaryColor),
+          headlineMedium: TextStyle(
+              fontSize: 18, color: Theme.of(context).primaryColorDark),
+          headlineSmall:
+              const TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
+        )),
       ),
       home: HomeView(),
     );
