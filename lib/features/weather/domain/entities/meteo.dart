07abc17b71753632a484d4a9429c4e04b3713271
Joseph Backelant's <backelantsjoseph@gmail.com>
2024-02-19 12:57:18 +0100

    Entity Meteo

---
+++ lib/features/weather/domain/entities/meteo.dart
@@ -0,0 +1,79 @@
+import 'package:equatable/equatable.dart';
+
+class MeteoEntity {
+  String cod;
+  int message;
+  int cnt;
+  List<Forecast> list;
+
+  MeteoEntity(
+      {required this.cod,
+      required this.message,
+      required this.cnt,
+      required this.list});
+}
+
+class Forecast {
+  int dt;
+  Main main;
+  List<Weather> weather;
+  Clouds clouds;
+  Wind wind;
+  int visibility;
+  String dt_txt;
+
+  Forecast(
+      {required this.dt,
+      required this.main,
+      required this.weather,
+      required this.clouds,
+      required this.wind,
+      required this.visibility,
+      required this.dt_txt});
+}
+
+class Main {
+  double temp;
+  double temp_min;
+  double temp_max;
+  double pressure;
+  double sea_level;
+  double grnd_level;
+  double humidity;
+  double temp_kf;
+
+  Main(
+      {required this.temp,
+      required this.temp_min,
+      required this.temp_max,
+      required this.pressure,
+      required this.sea_level,
+      required this.grnd_level,
+      required this.humidity,
+      required this.temp_kf});
+}
+
+class Weather {
+  int id;
+  String main;
+  String description;
+  String icon;
+
+  Weather(
+      {required this.id,
+      required this.main,
+      required this.description,
+      required this.icon});
+}
+
+class Clouds {
+  int all;
+  Clouds({required this.all});
+}
+
+class Wind {
+  double speed;
+  int deg;
+  double gust;
+  Wind({required this.speed, required this.deg, required this.gust});
+}
